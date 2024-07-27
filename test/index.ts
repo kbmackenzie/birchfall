d2f9f5a37b85161457ea2a1b102a660c196d694c
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-27 17:46:57 -0300

    test: Add more parser tests

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,5 @@
 import { test, logTest } from './parser-test';
-import { many, word, choice } from '@/parser'
+import { Parser, pure, bind, then, char, many, word, choice, choices, anyChar } from '@/parser'
 
 { 
   const input  = 'hellohellohellohelloworldworld';
@@ -12,6 +12,41 @@ import { many, word, choice } from '@/parser'
   const result = test(
     parser(input),
     result => result.length === 6
+  );
+  logTest(result);
+}
+
+{ 
+  const input  = 'do re and me';
+
+  const note = choices(
+    word('do'),
+    word('re'),
+    word('mi'),
+    word('fa'),
+    word('sol'),
+    word('la'),
+    word('si'),
+  );
+  const spaces = many(char(' '));
+  const noteParser = bind(
+    note,
+    (n) => then(spaces, pure(n))
+  );
+  const parser: Parser<[string[], string]> = bind(
+    many(noteParser),
+    (ns) => bind(
+      many(anyChar()),
+      (rest) => pure([ns, rest.join('')])
+    )
+  );
+
+  const result = test(
+    parser(input),
+    result => result[0].length === 2
+      && result[0][0] === 'do'
+      && result[0][1] === 're'
+      && result[1] === 'and me'
   );
   logTest(result);
 }
