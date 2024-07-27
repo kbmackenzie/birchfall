a168acab8c408f59977937c058ac966702b7f5c4
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-27 09:33:49 -0300

    test: Rewrite parser test

--- test/index.ts
+++ test/index.ts
@@ -1,18 +1,17 @@
+import { test, logTest } from './parser-test';
 import { many, word, choice } from '@/parser'
 
-const parser = many(
-  choice(
-    word('hello'),
-    word('world'),
-  )
-);
-
-const input  = 'hellohellohellohelloworldworld';
-const output = parser(input);
-
-console.log(`Input: ${input}`);
-console.log(`Output: { type: ${output.type}, value: ${output.type === 'ok' ? output.value : 'none'} }`);
-
-if (output.type !== 'ok' || output.value.length !== 6) {
-  throw new Error('Test failed!');
+{ 
+  const input  = 'hellohellohellohelloworldworld';
+  const parser = many(
+    choice(
+      word('hello'),
+      word('world'),
+    )
+  );
+  const result = test(
+    parser(input),
+    result => result.length === 6
+  );
+  logTest(result);
 }
