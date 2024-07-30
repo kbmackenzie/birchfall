438ad07ff247d631b15b62f3b61e1b1b84f74e5a
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-30 09:27:54 -0300

    chore: Add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+  },
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist/'
+  ],
+};
